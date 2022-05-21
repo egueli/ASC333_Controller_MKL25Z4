7c0b90500bdbf5a7e2778f51a1614c3eaa886d5f
Enrico Gueli <enrico.gueli@gmail.com>
2022-05-21 17:35:55 +0200

    add basic SPI init & data send code

--- source/led_column_driver.c
+++ source/led_column_driver.c
@@ -7,7 +7,6 @@
 
 #include "led_column_driver.h"
 
-#include "fsl_debug_console.h"
 
 /* FreeRTOS kernel includes. */
 #include "FreeRTOS.h"
@@ -15,13 +14,105 @@
 #include "queue.h"
 #include "timers.h"
 
+/* Freescale includes. */
+#include "fsl_debug_console.h"
+#include "fsl_spi.h"
+#include "fsl_spi_freertos.h"
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+
+// The column shift registers are connected to SPI0.
+#define COLUMN_DRIVER_SPI_MASTER_BASE (SPI0_BASE)
+#define COLUMN_DRIVER_SPI_MASTER_IRQN (SPI0_IRQn)
+#define SPI_MASTER_CLK_SRC (SPI0_CLK_SRC)
+#define SPI_MASTER_CLK_FREQ CLOCK_GetFreq((SPI0_CLK_SRC))
+
+#define COLUMN_DRIVER_SPI_MASTER_BASEADDR ((SPI_Type *)COLUMN_DRIVER_SPI_MASTER_BASE)
+#define SPI_NVIC_PRIO 2
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+static bool initSpi();
+static void sendData();
+
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+static spi_rtos_handle_t master_rtos_handle;
+
 
 /*!
  * @brief Task responsible for controlling the LED column drivers.
  */
 void led_column_driver_task(void *pvParameters)
 {
-    PRINTF("Hi, I'm the task controlling the LED column drivers. I will do absolutely anything for now. And now I leave.\n");
+	if(!initSpi()) {
+        vTaskSuspend(NULL);
+        return;
+	}
 
+	while(true) {
+		sendData();
+	}
 	vTaskSuspend(NULL);
+}
+
+
+static bool initSpi() {
+    NVIC_SetPriority(COLUMN_DRIVER_SPI_MASTER_IRQN, SPI_NVIC_PRIO);
+
+    /*
+     * masterConfig.enableStopInWaitMode = false;
+     * masterConfig.polarity = kSPI_ClockPolarityActiveHigh;
+     * masterConfig.phase = kSPI_ClockPhaseFirstEdge;
+     * masterConfig.direction = kSPI_MsbFirst;
+     * masterConfig.dataMode = kSPI_8BitMode;
+     * masterConfig.txWatermark = kSPI_TxFifoOneHalfEmpty;
+     * masterConfig.rxWatermark = kSPI_RxFifoOneHalfFull;
+     * masterConfig.pinMode = kSPI_PinModeNormal;
+     * masterConfig.outputMode = kSPI_SlaveSelectAutomaticOutput;
+     * masterConfig.baudRate_Bps = 500000U;
+     */
+    spi_master_config_t masterConfig;
+    SPI_MasterGetDefaultConfig(&masterConfig);
+    masterConfig.baudRate_Bps = 500000;
+
+    uint32_t sourceClock = SPI_MASTER_CLK_FREQ;
+    status_t status;
+    status = SPI_RTOS_Init(&master_rtos_handle, COLUMN_DRIVER_SPI_MASTER_BASEADDR, &masterConfig, sourceClock);
+
+    if (status != kStatus_Success)
+    {
+        PRINTF("DSPI master: error during initialization. \r\n");
+        return false;
+    }
+
+    return true;
+}
+
+static void sendData() {
+	const size_t kBufferSize = 64;
+	uint8_t txBuff[kBufferSize];
+	uint8_t rxBuff[kBufferSize]; // won't use this one
+
+    spi_transfer_t masterXfer = {
+    		.txData = txBuff,
+			.rxData = rxBuff,
+			.dataSize = kBufferSize
+    };
+
+    /* Init Buffer */
+    for (uint8_t i = 0; i < kBufferSize; i++)
+    {
+    	txBuff[i] = i;
+    }
+
+    status_t status = SPI_RTOS_Transfer(&master_rtos_handle, &masterXfer);
+    if (status != kStatus_Success)
+    {
+        PRINTF("SPI transfer completed with error. \r\n");
+    }
 }
