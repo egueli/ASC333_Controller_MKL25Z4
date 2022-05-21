3e6fb3b930aea0228bf0983451f225a1ee01ba8d
Enrico Gueli <enrico.gueli@gmail.com>
2022-05-21 09:15:38 +0200

    Add basic FreeRTOS task

--- source/MKL25Z4_Project.c
+++ source/MKL25Z4_Project.c
@@ -1,33 +1,3 @@
-/*
- * Copyright 2016-2022 NXP
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
- *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- *
- * o Neither the name of NXP Semiconductor, Inc. nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 /**
  * @file    MKL25Z4_Project.c
  * @brief   Application entry point.
@@ -39,9 +9,26 @@
 #include "clock_config.h"
 #include "MKL25Z4.h"
 #include "fsl_debug_console.h"
-/* TODO: insert other include files here. */
+
+/* FreeRTOS kernel includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "timers.h"
 
 /* TODO: insert other definitions and declarations here. */
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+
+static void master_task(void *pvParameters);
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+/* Task priorities. */
+#define master_task_PRIORITY (configMAX_PRIORITIES - 1)
 
 /*
  * @brief   Application entry point.
@@ -66,16 +53,21 @@ int main(void) {
     BOARD_InitDebugConsole();
 #endif
 
-    PRINTF("Hello World\n");
+    PRINTF("ASC 333 Controller - Enrico Gueli 2022\n");
 
-    /* Force the counter to be placed into memory. */
-    volatile static int i = 0 ;
-    /* Enter an infinite loop, just incrementing a counter. */
-    while(1) {
-        i++ ;
-        /* 'Dummy' NOP to allow source level single stepping of
-            tight while() loop */
-        __asm volatile ("nop");
-    }
-    return 0 ;
+    xTaskCreate(master_task, "Master_task", configMINIMAL_STACK_SIZE + 38, NULL, master_task_PRIORITY, NULL);
+
+    vTaskStartScheduler();
+    for (;;)
+        ;
+}
+
+/*!
+ * @brief Task responsible for master SPI communication.
+ */
+static void master_task(void *pvParameters)
+{
+    PRINTF("Hi, I'm the master task. I will do absolutely anything. And now I leave.\n");
+
+	vTaskSuspend(NULL);
 }
